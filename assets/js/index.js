880cc5861cb9ece9609778deae667af8263941ba
Claudia Villarroel <claudia.villarroel@live.com>
2023-07-08 14:05:12 -0400

    ADD: Reto1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,16 @@
 /*RETO 1*/
+let imagenTieneBorde = false;
 
+function bordeImagen(){
+    let imagen = document.getElementById("reto1_img");
+    if(imagenTieneBorde == false){
+        imagen.style.border = "2px solid red";
+        imagenTieneBorde = true;
+    }else{
+        imagenTieneBorde = false;
+        imagen.style.border = "none";
+    }
+}
 
 
 /*RETO 2*/
