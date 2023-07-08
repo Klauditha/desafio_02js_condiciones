af826c44e2e4fa7abcb345d8bc8aa668263b2158
Claudia Villarroel <claudia.villarroel@live.com>
2023-07-08 14:40:21 -0400

    WIP: Reto2

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,9 +12,18 @@ function bordeImagen(){
     }
 }
 
-
 /*RETO 2*/
-
-
-
+let input_sticker1 = document.getElementById("input_sticker1").value;
+let input_sticker2 = document.getElementById("input_sticker2").value;
+let input_sticker3 = document.getElementById("input_sticker3").value;
+let resultado_text = document.getElementById("reto2_resultado").innerHTML;
+function verificarCantidadStickers(){
+    let suma = input_sticker1 + input_sticker2 +input_sticker3;
+    if(suma>=10){
+        resultado_text = "Llevas "+ suma + " stickers";
+    }
+    else{
+        resultado_text = "Llevas demasiados stickers";
+    }
+}
 /*RETO 3*/
